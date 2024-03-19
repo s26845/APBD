f491306119ddee12fe186c9b18f4d389dc6a33cb
s26845 <s26845@pjwstk.edu.pl>
2024-03-19 05:47:41 +0100

    Modification 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+double a, b;
+a = double.Parse(Console.ReadLine());
+b = double.Parse(Console.ReadLine());
+double result = (a + b) / 2;
+Console.WriteLine(result);
